df91bdf8b95c90060501e294b1eaf60ccef5d00e
dzintars-magons <dzintars.magons@gmail.com>
2021-03-05 23:36:31 +0200

    update

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,18 @@ function something (){
     let y = 10; 
 }
 
+const burgerOpen = document.querySelector("#burger-open");
+const burgerClose = document.querySelector("#burger-close");
+const burgerMenu = document.querySelector("#burger-menu");
+
+burgerOpen.addEventListener("click", function onclick(event) {
+    event.preventDefault();
+    burgerMenu.style.display = "block";
+  });
+
+burgerClose.addEventListener("click", function onclick(event) {
+    event.preventDefault();
+    burgerMenu.style.display = "none";
+
+});
+
